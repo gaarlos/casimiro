6e1b9396b403071cf8587af479d349d8e78a4749
Garlos <gaarlos@users.noreply.github.com>
2017-11-22 20:15:15 +0100

    Fixed some problems with answers
    Also added a simple print to know who sent the message and its content.

--- Casimiro.py
+++ Casimiro.py
@@ -8,8 +8,12 @@ def listener(messages):
   for m in messages:
 
     if m.content_type == 'text':
-      
-      bot.send_message(m.chat_id, 'No me molestes...')
+
+      print("[" + str(m.chat.id) + "]: " + m.text)
+
+      bot.send_message(m.chat.id, 'No me molestes...')
+
+bot.set_update_listener(listener)
 
 @bot.message_handler(commands=['start'])
 
